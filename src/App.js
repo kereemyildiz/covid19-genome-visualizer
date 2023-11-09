943a0c2b38c88b6a8b9d06b917755ef6ca412b35
kereemyildiz <a.keremyildiz99@gmail.com>
2023-11-09 22:28:07 +0300

    create context and store random genome

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useContext, useEffect } from "react";
 import "./App.css";
+// import { generateRandomSequence } from "./helpers/fooGenomeElements";
+import { SequenceContext } from "./contexts/GenomeContext";
+import RandomSequence from "./components/RandomSequence";
+import { createArrayOfDictionaries } from "./data/possibilityMap";
+import { main } from "./helpers/fooGenomeElements";
 
 function App() {
+	const { generateRandomGenome, randomGenome } = useContext(SequenceContext);
+	useEffect(() => {
+		console.log("1");
+		generateRandomGenome();
+	}, []);
+
+	useEffect(() => {
+		main(randomGenome);
+	}, [randomGenome]);
 	return (
 		<div className="App">
-			<header className="App-header"></header>
+			<RandomSequence />
+			{console.log("22", randomGenome)}
 		</div>
 	);
 }
---
+++ src/constants/constantVars.js
@@ -0,0 +1,4 @@
+const nucleotides = ["A", "T", "G", "C"];
+const windowSize = 5;
+
+export { nucleotides, windowSize };
