013d8abbe94ff0904352f37eb712f9a86a1ad6e2
kereemyildiz <a.keremyildiz99@gmail.com>
2023-11-11 19:08:03 +0300

    app moved to redux-toolkit

---
+++ src/features/genome/genomeSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { generateRandomSequence, main } from "../../helpers/fooGenomeElements";
+
+const initialState = {
+	randomSeq: null,
+	possibilityMap: null,
+};
+
+export const genomeSlice = createSlice({
+	name: "genome",
+	initialState,
+	reducers: {
+		generate: (state) => {
+			console.log("run");
+			state.randomSeq = generateRandomSequence();
+			state.possibilityMap = main(state.randomSeq);
+		},
+	},
+});
+
+export const { generate } = genomeSlice.actions;
+export default genomeSlice.reducer;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { GenomeProvider } from "./contexts/GenomeContext";
+import { Provider } from "react-redux";
+import { store } from "./states/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<GenomeProvider>
+	<Provider store={store}>
 		<App />
-	</GenomeProvider>
+	</Provider>
 );
