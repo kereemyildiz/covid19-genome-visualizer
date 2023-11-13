0bb83ef1b2d9e99d673f467a8ec5e9e6a38e75c9
kereemyildiz <a.keremyildiz99@gmail.com>
2023-11-13 12:00:17 +0300

    remove extra comments

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -67,16 +67,14 @@ function BarChart({ data }) {
 }
 
 function getColorForNucleotide(nucleotide) {
-	// Define colors for A, C, T, G
 	const colorMap = {
-		A: "#FF5733", // Replace with your color for A
-		C: "#3399FF", // Replace with your color for C
-		T: "#33CC33", // Replace with your color for T
-		G: "#9966FF", // Replace with your color for G
+		A: "#FF5733",
+		C: "#3399FF",
+		T: "#33CC33",
+		G: "#9966FF",
 	};
 
-	// Return the color for the given nucleotide
-	return colorMap[nucleotide] || "#000000"; // Default to black if color not found
+	return colorMap[nucleotide] || "#000000";
 }
 
 export default BarChart;
