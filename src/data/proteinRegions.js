3cdc0aa3558d2550177d992989ded797fe8cd670
kereemyildiz <a.keremyildiz99@gmail.com>
2023-12-16 17:46:56 +0300

    update protein region

--- src/data/proteinRegions.js
+++ src/data/proteinRegions.js
@@ -1,14 +1,14 @@
 // export const proteinRegions = {
 // 	ORF1ab: "0-21562",
 // 	S: "21563-25393",
-// 	ORF3a: "25393-26245",
-// 	E: "26245-26523",
-// 	M: "26523-27201",
-// 	ORF6: "27201-27393",
+// 	ORF3a: "25394-26245",
+// 	E: "26246-26523",
+// 	M: "26524-27201",
+// 	ORF6: "27202-27393",
 // 	ORF7a: "27394-27892",
 // 	ORF8: "27893-28273",
 // 	N: "28274-29558",
-// 	ORF10: "29558-30000",
+// 	ORF10: "29559-29999",
 // };
 
 export const proteinRegions = {
@@ -17,12 +17,29 @@ export const proteinRegions = {
 	ORF3a: "460-510",
 	E: "511-600",
 	M: "601-630",
-	ORF6: "631-669",
+	ORF6: "631-699",
 	ORF7a: "700-799",
 	ORF8: "800-849",
 	N: "850-899",
 	ORF10: "900-999",
 };
+
+export const proteinRegionsSize = getProteinRegionSize();
+
+function getProteinRegionSize() {
+	let a = {};
+	Object.entries(proteinRegions).map((item) => {
+		const [protein, range] = item;
+		const [start, end] = range.split("-").map(Number);
+		const length = end - start + 1;
+		a[protein] = length;
+		return {
+			protein,
+			length,
+		};
+	});
+	return a;
+}
 
 // ORF1ab: { interval: "0-399", color: "rgba(255, 99, 132, 0.5)" },
 // S: { interval: "400-459", color: "rgba(54, 102, 235, 0.5)" },
