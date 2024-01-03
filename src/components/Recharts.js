d7e59b840bd62140a190a27052b41458dfb15d17
kereemyildiz <a.keremyildiz99@gmail.com>
2024-01-04 02:55:43 +0300

    try plotly.js

--- src/components/Recharts.js
+++ src/components/Recharts.js
@@ -1,99 +1,115 @@
-import React, { useEffect, useRef } from "react";
-import * as d3 from "d3";
+import React, { useState, useEffect } from "react";
+import Plot from "react-plotly.js";
 
 const GenomeChart = ({ genomeData, genomeSequence }) => {
-	const chartRef = useRef(null);
-	const svgRef = useRef(null);
+	const [decimatedData, setDecimatedData] = useState([]);
+	const [currentView, setCurrentView] = useState({ start: 0, end: 1000 }); // Default view
+	const decimationFactor = 30; // Define the decimation factor
+	const zoomThreshold = 10; // Define a zoom threshold for switching between raw and decimated data
 
 	useEffect(() => {
-		if (genomeData && genomeSequence) {
-			drawChart();
+		if (genomeData && genomeData.length > 0 && genomeSequence) {
+			const decimated = decimateData(genomeData, decimationFactor);
+			setDecimatedData(decimated);
 		}
 	}, [genomeData, genomeSequence]);
 
-	const drawChart = () => {
-		// Clear previous SVG elements
-		d3.select(svgRef.current).selectAll("*").remove();
-
-		const nucleotides = ["A", "C", "G", "T"];
-		const processedData = genomeSequence.split("").map((nt, index) => {
-			return nucleotides.reduce(
-				(acc, nucleotide, i) => {
-					acc[nucleotide] = genomeData[i][index];
-					return acc;
-				},
-				{ position: index }
+	const decimateData = (data, factor) => {
+		const decimated = [];
+		for (let i = 0; i < data[0].length; i += factor) {
+			const chunk = data.map((d) => d.slice(i, i + factor));
+			const averagedChunk = chunk.map(
+				(d) => d.reduce((a, b) => a + b, 0) / factor
 			);
-		});
-
-		const margin = { top: 20, right: 30, bottom: 30, left: 40 };
-		const fullWidth = 3000; // Full width of the chart
-		const width = chartRef.current.clientWidth - margin.left - margin.right; // Viewport width
-		const height = 500 - margin.top - margin.bottom;
-
-		const x = d3
-			.scaleBand()
-			.range([0, fullWidth])
-			.padding(0.1)
-			.domain(processedData.map((d) => d.position));
-
-		const y = d3.scaleLinear().range([height, 0]).domain([0, 1]);
-
-		const svg = d3
-			.select(svgRef.current)
-			.attr(
-				"viewBox",
-				`0 0 ${fullWidth + margin.left + margin.right} ${
-					height + margin.top + margin.bottom
-				}`
-			)
-			.append("g")
-			.attr("transform", `translate(${margin.left},${margin.top})`);
-
-		const stack = d3.stack().keys(nucleotides)(processedData);
-
-		svg
-			.selectAll(".stack")
-			.data(stack)
-			.enter()
-			.append("g")
-			.attr("fill", (d, i) => d3.schemeCategory10[i])
-			.selectAll("rect")
-			.data((d) => d)
-			.enter()
-			.append("rect")
-			.attr("x", (d) => x(d.data.position))
-			.attr("y", (d) => y(d[1]))
-			.attr("height", (d) => Math.max(0, y(d[0]) - y(d[1]))) // Ensure non-negative height
-			.attr("width", x.bandwidth());
-
-		svg
-			.append("g")
-			.attr("transform", `translate(0,${height})`)
-			.call(d3.axisBottom(x).tickValues([]));
-
-		svg.append("g").call(d3.axisLeft(y));
-
-		// Zoom and Pan
-		const zoom = d3
-			.zoom()
-			.scaleExtent([1, 5])
-			.translateExtent([
-				[0, 0],
-				[fullWidth, height],
-			])
-			.on("zoom", (event) => {
-				svg.attr("transform", event.transform);
-			});
-
-		d3.select(svgRef.current).call(zoom);
+			decimated.push(averagedChunk);
+		}
+		return decimated;
 	};
 
+	const handleZoom = (event) => {
+		const xAxis = event["xaxis.range[0]"]
+			? Math.max(Math.floor(event["xaxis.range[0]"]), 0)
+			: 0;
+		const xAxisEnd = event["xaxis.range[1]"]
+			? Math.min(Math.ceil(event["xaxis.range[1]"]), genomeSequence.length)
+			: genomeSequence.length;
+		const zoomLevel = genomeSequence.length / (xAxisEnd - xAxis);
+
+		if (zoomLevel > zoomThreshold && xAxisEnd - xAxis < genomeSequence.length) {
+			setCurrentView({ start: xAxis, end: xAxisEnd });
+		} else {
+			setCurrentView({
+				start: 0,
+				end: genomeSequence.length / decimationFactor,
+			});
+		}
+	};
+	const traces =
+		genomeData && genomeSequence && decimatedData.length > 0
+			? getTraces(currentView, genomeData, decimatedData, genomeSequence)
+			: [];
+
+	const layout = {
+		title: "Genome Sequence Mutation Visualization",
+		barmode: "stack",
+		xaxis: {
+			title: "Position",
+			rangeselector: {
+				buttons: [
+					{
+						count: 1,
+						label: "1m",
+						step: "month",
+						stepmode: "backward",
+					},
+					{
+						step: "all",
+					},
+				],
+			},
+			rangeslider: { range: [0, 1000] }, // Initial range for x-axis
+			type: "linear",
+		},
+		yaxis: {
+			fixedrange: false, // Allow y-axis range to change (enables vertical panning)
+			title: "Mutation Probability",
+		},
+		dragmode: "pan", // Enable panning by dragging
+	};
 	return (
-		<div ref={chartRef} style={{ width: "100%", overflowX: "auto" }}>
-			<svg ref={svgRef}></svg>
-		</div>
+		<Plot
+			data={traces}
+			layout={layout}
+			onRelayout={handleZoom}
+			style={{ width: "100%", height: "100%" }}
+		/>
 	);
+};
+
+const getTraces = (view, rawGenomeData, decimatedData, genomeSequence) => {
+	if (!rawGenomeData || !decimatedData || !genomeSequence) {
+		return []; // Ensure that data is available
+	}
+
+	const nucleotides = ["A", "C", "G", "T"];
+	const useRawData = view.end - view.start < genomeSequence.length;
+	const data = useRawData ? rawGenomeData : decimatedData;
+	const xValues = useRawData
+		? Array.from(
+				{ length: view.end - view.start },
+				(_, i) => i + view.start + 1
+		  )
+		: Array.from({ length: data[0].length }, (_, i) => i + 1);
+
+	return nucleotides.map((nucleotide, index) => {
+		return {
+			x: xValues,
+			y: data[index],
+			name: nucleotide,
+			type: "bar",
+			barmode: "stack",
+		};
+	});
 };
 
 export default GenomeChart;
