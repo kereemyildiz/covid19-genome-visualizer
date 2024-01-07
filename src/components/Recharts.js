7d336f011c22a69428e67aaefb629cebfa330ba8
kereemyildiz <a.keremyildiz99@gmail.com>
2024-01-08 01:56:19 +0300

    implement sidepanel to show selected protein region as annotation

--- src/components/Recharts.js
+++ src/components/Recharts.js
@@ -1,66 +1,122 @@
 import React, { useEffect, useRef, useState } from "react";
 import Chart from "chart.js/auto";
+import annotationPlugin from "chartjs-plugin-annotation";
 import { getColorForNucleotide, nucleotides } from "../helpers/helperFunctions";
-import {
-	determineProteinRegion,
-	proteinMidPoints,
-	proteinRegionLabels,
-} from "../helpers/fooGenomeElements";
+import { determineProteinRegion } from "../helpers/fooGenomeElements";
 import { proteinRegions } from "../data/proteinRegions";
 import {
 	proteinRegionColorMap,
 	proteinRegionColorMapAnnotations,
 } from "../utils/proteinRegionColorMap";
-import annotationPlugin from "chartjs-plugin-annotation";
+import SidePanel from "./SidePanel";
 
 Chart.register(annotationPlugin);
 
 const GenomeChart = ({ genomeData, genomeSequence }) => {
 	const chartRef = useRef(null);
+	const [activeProtein, setActiveProtein] = useState(null);
+	const [chart, setChart] = useState(null);
 
-	const decimateData = (data, factor) => {
-		return data.map((dataset) =>
-			dataset.reduce((acc, curr, index) => {
-				if (index % factor === 0) {
-					const slice = dataset.slice(index, index + factor);
-					const sum = slice.reduce((sum, value) => sum + value, 0);
-					acc.push(sum / factor);
-				}
-				return acc;
-			}, [])
-		);
-	};
+	const decimateFactor = 15;
+	const decimatedData = genomeData.map((dataset) =>
+		dataset.reduce((acc, curr, index) => {
+			if (index % decimateFactor === 0) {
+				const slice = dataset.slice(index, index + decimateFactor);
+				const sum = slice.reduce((sum, value) => sum + value, 0);
+				acc.push(sum / decimateFactor);
+			}
+			return acc;
+		}, [])
+	);
 
 	useEffect(() => {
-		const decimateFactor = 15;
-		const decimatedData = decimateData(genomeData, decimateFactor);
-		const allSequenceLabels = genomeSequence.split("").map((n) => n);
-		const proteinRegionLabels = allSequenceLabels.map((_, idx) =>
-			determineProteinRegion(idx)
-		);
-		const chunkViewLabels = decimatedData[0].map((_, idx) => `Chunk ${idx}`);
+		const ctx = chartRef.current.getContext("2d");
+		if (!ctx) return;
 
-		const proteinRegionChunkViewLabels = chunkViewLabels.map((_, idx) =>
-			determineProteinRegion(idx, decimateFactor)
-		);
-		const currentChartRef = chartRef.current;
+		const labels = decimatedData[0].map((_, idx) => `Chunk ${idx}`);
 
-		console.log("as", proteinRegionChunkViewLabels);
-		console.log(proteinRegionLabels);
-		console.log("pr mid:", proteinMidPoints);
+		const datasets = decimatedData.map((dataset, idx) => ({
+			label: nucleotides[idx],
+			data: dataset,
+			borderColor: getColorForNucleotide(nucleotides[idx]),
+			backgroundColor: getColorForNucleotide(nucleotides[idx]),
+		}));
 
-		const annotations = Object.keys(proteinRegions).map((key) => {
+		const data = {
+			labels: labels,
+			datasets: datasets,
+		};
+
+		const options = {
+			animation: false,
+			responsive: true,
+			scales: {
+				x: {
+					stacked: true,
+					ticks: {
+						font: {
+							weight: "bold",
+						},
+					},
+				},
+				y: {
+					stacked: true,
+				},
+			},
+			plugins: {
+				annotation: {
+					annotations: createAnnotations(activeProtein),
+				},
+				zoom: {
+					zoom: {
+						wheel: {
+							enabled: true,
+						},
+						pinch: {
+							enabled: true,
+						},
+						mode: "x",
+					},
+					pan: {
+						enabled: true,
+						mode: "x",
+					},
+				},
+			},
+		};
+
+		const chartInstance = new Chart(ctx, {
+			type: "bar",
+			data: data,
+			options: options,
+		});
+
+		setChart(chartInstance);
+
+		return () => chartInstance.destroy();
+	}, []);
+
+	useEffect(() => {
+		if (chart) {
+			chart.options.plugins.annotation.annotations =
+				createAnnotations(activeProtein);
+			chart.update();
+		}
+	}, [activeProtein, chart]);
+
+	const createAnnotations = (activeProtein) => {
+		return Object.keys(proteinRegions).map((key) => {
 			const range = proteinRegions[key];
 			const [start, end] = range
 				.split("-")
 				.map((e) => parseInt(e / decimateFactor));
 
 			return {
-				display: true,
+				display: activeProtein === key,
 				type: "box",
 				xMin: start,
 				xMax: end,
-				backgroundColor: proteinRegionColorMapAnnotations[key], // You can use different colors for each region
+				backgroundColor: proteinRegionColorMap[key],
 				borderColor: proteinRegionColorMap[key],
 				borderWidth: 2,
 				label: {
@@ -70,120 +126,28 @@ const GenomeChart = ({ genomeData, genomeSequence }) => {
 				},
 			};
 		});
-		console.log("ann:", annotations);
-		if (currentChartRef) {
-			const ctx = currentChartRef.getContext("2d");
+	};
 
-			if (ctx) {
-				const labels = chunkViewLabels;
-				const datasets = decimatedData.map((dataset, idx) => ({
-					label: nucleotides[idx],
-					data: dataset,
-					borderColor: getColorForNucleotide(nucleotides[idx]),
-					backgroundColor: getColorForNucleotide(nucleotides[idx]),
-				}));
+	const handleProteinHover = (protein) => {
+		setActiveProtein(protein);
+	};
 
-				const data = {
-					labels: labels,
-					datasets: datasets,
-				};
-
-				const options = {
-					animation: false,
-					responsive: true,
-					scales: {
-						x: {
-							stacked: true,
-							ticks: {
-								font: {
-									weight: "bold",
-								},
-							},
-						},
-						x2: {
-							type: "category",
-							labels: proteinRegionChunkViewLabels,
-							ticks: {
-								callback: function (value, index, values) {
-									return value;
-								},
-							},
-							// ticks: {
-							// 	maxTicksLimit: 10,
-							// },
-						},
-
-						// x3: {
-						// 	border: {
-						// 		display: true,
-						// 	},
-						// 	grid: {
-						// 		display: true,
-						// 	},
-						// 	labels: decimatedData[0]
-						// 		.map((_, idx) => `AAA ${idx}`)
-						// 		.slice(0, 10),
-						// },
-
-						y: {
-							stacked: true,
-						},
-					},
-					plugins: {
-						annotation: {
-							annotations: annotations,
-						},
-						zoom: {
-							zoom: {
-								wheel: {
-									enabled: true,
-								},
-								pinch: {
-									enabled: true,
-								},
-								mode: "x",
-							},
-							pan: {
-								enabled: true,
-								mode: "x",
-								onPan: () => {
-									console.log("panned");
-								},
-							},
-						},
-					},
-				};
-
-				const config = {
-					type: "bar",
-					data: data,
-					options: options,
-				};
-
-				const chart = new Chart(ctx, config);
-
-				return () => chart.destroy();
-			}
-		}
-	}, [genomeData]);
+	const handleProteinLeave = () => {
+		setActiveProtein(null);
+	};
 
 	return (
-		<div className="container flex">
-			<div className="chart-area w-[80vw] h-[80vh]">
+		<div className="chart-container flex">
+			<SidePanel
+				proteinRegions={proteinRegions}
+				onProteinHover={handleProteinHover}
+				onProteinLeave={handleProteinLeave}
+			/>
+			<div className="chart-area">
 				<canvas ref={chartRef} />
 			</div>
 		</div>
 	);
 };
-
-// Add this to your chart options
-// const options = {
-// 	// ...
-// 	plugins: {
-// 		annotation: {
-// 			annotations: annotations,
-// 		},
-// 	},
-// };
 
 export default GenomeChart;
