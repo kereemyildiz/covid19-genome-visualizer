2c8aa29d9e43efefb134be143cc6609eaf564481
kereemyildiz <a.keremyildiz99@gmail.com>
2024-01-08 05:49:26 +0300

    add custom sublabel plugin to show protein regions

--- src/components/Recharts.js
+++ src/components/Recharts.js
@@ -3,7 +3,7 @@ import Chart from "chart.js/auto";
 import annotationPlugin from "chartjs-plugin-annotation";
 import { getColorForNucleotide, nucleotides } from "../helpers/helperFunctions";
 import { determineProteinRegion } from "../helpers/fooGenomeElements";
-import { proteinRegions } from "../data/proteinRegions";
+import { proteinRegions, proteinRegionsSize } from "../data/proteinRegions";
 import {
 	proteinRegionColorMap,
 	proteinRegionColorMapAnnotations,
@@ -13,6 +13,56 @@ import { Switch } from "@material-tailwind/react";
 import { getRelativePosition } from "chart.js/helpers";
 
 Chart.register(annotationPlugin);
+function proteinRegionByPercentage() {
+	const total = Object.values(proteinRegionsSize).reduce(
+		(acc, curr) => acc + curr,
+		0
+	);
+	console.log("total: ", total);
+	return Object.entries(proteinRegionsSize).map((item) => {
+		const [protein, length] = item;
+		const percentage = (length / total) * 100;
+		console.log("percentage:", percentage);
+		return {
+			protein,
+			percentage,
+		};
+	});
+}
+console.log("asd:", proteinRegionByPercentage());
+
+const subLabels = {
+	id: "subLabels",
+	afterDatasetsDraw(chart, args, pluginOptions) {
+		const {
+			ctx,
+			chartArea: { left, right, top, bottom, width, height },
+		} = chart;
+		ctx.save();
+
+		subLabelText("ORF1ab", (width / 100) * 40);
+		subLabelText("S", (width / 100) * 78);
+		// subLabelText("ORF3a", (width / 100) * 78);
+		// subLabelText("ORF3a", (width / 100) * 78);
+		// subLabelText("E", (width / 100) * 78);
+		// subLabelText("M", (width / 100) * 78);
+		// subLabelText("ORF6", (width / 100) * 78);
+		// subLabelText("ORF7a", (width / 100) * 78);
+		// subLabelText("ORF7b", (width / 100) * 78);
+		// subLabelText("ORF8", (width / 100) * 78);
+		// subLabelText("N", (width / 100) * 78);
+		subLabelText("ORF10", (width / 100) * 97);
+
+		function subLabelText(text, x, y = 100) {
+			ctx.font = "bolder 12px sans-serif";
+			ctx.fillStyle = "rgba(102, 102, 102, 1)";
+			ctx.textAlign = "center";
+			ctx.fillText(text, x + left, bottom + y);
+		}
+	},
+};
+Chart.register(subLabels);
+console.log(proteinRegionsSize);
 
 const GenomeChart = ({ genomeData, genomeSequence }) => {
 	const chartRef = useRef(null);
@@ -92,6 +142,11 @@ const GenomeChart = ({ genomeData, genomeSequence }) => {
 					},
 					y: {
 						stacked: true,
+					},
+				},
+				layout: {
+					padding: {
+						bottom: 30,
 					},
 				},
 				plugins: {
@@ -186,7 +241,7 @@ const GenomeChart = ({ genomeData, genomeSequence }) => {
 	};
 	return (
 		<div className="chart-container flex">
-			<div className=" w-[100vw] h-[100vh]">
+			<div className=" w-[100vw] h-[80vh]">
 				<canvas ref={chartRef} />
 			</div>
 			<div>
--- src/utils/proteinRegionColorMap.js
+++ src/utils/proteinRegionColorMap.js
@@ -11,17 +11,17 @@ export const proteinRegionColorMap = {
 	N: "rgba(55, 26, 86, 0.5)",
 	ORF10: "rgba(75, 102, 142, 0.5)",
 };
-
+const alpha = 0.25;
 export const proteinRegionColorMapAnnotations = {
-	ORF1ab: "rgba(255, 99, 132, 0.1)",
-	S: "rgba(54, 102, 205, 0.1)",
-	ORF3a: "rgba(255, 206, 86, 0.1)",
-	E: "rgba(75, 192, 192, 0.1)",
-	M: "rgba(153, 102, 255, 0.1)",
-	ORF6: "rgba(255, 159, 64, 0.1)",
-	ORF7a: "rgba(155, 23, 112, 0.1)",
-	ORF7b: "rgba(240, 65, 102, 0.1)",
-	ORF8: "rgba(54, 162, 235, 0.1)",
-	N: "rgba(55, 26, 86, 0.1)",
-	ORF10: "rgba(75, 102, 142, 0.1)",
+	ORF1ab: `rgba(255, 99, 132, ${alpha})`,
+	S: `rgba(54, 102, 205, ${alpha})`,
+	ORF3a: `rgba(255, 206, 86, ${alpha})`,
+	E: `rgba(75, 192, 192, ${alpha})`,
+	M: `rgba(153, 102, 255, ${alpha})`,
+	ORF6: `rgba(255, 159, 64, ${alpha})`,
+	ORF7a: `rgba(155, 23, 112, ${alpha})`,
+	ORF7b: `rgba(240, 65, 102, ${alpha})`,
+	ORF8: `rgba(54, 162, 235, ${alpha})`,
+	N: `rgba(55, 26, 86, ${alpha})`,
+	ORF10: `rgba(75, 102, 142, ${alpha})`,
 };
